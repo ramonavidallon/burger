3b60bd327555ad29ab134c1ecefcaac73dd0ffe3
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-06-03 23:36:32 -0700

    css edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bonzai Veggie Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Primehouse Chop Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burnin Love Burger', true);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Bonzai Veggie Burger', true);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Primehouse Chop Burger', true);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Burnin Love Burger', true);
